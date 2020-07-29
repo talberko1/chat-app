5b8865b6a845afaa6a38529344d37deee7825d16
Tal Berko <talber461@gmail.com>
2020-07-30 00:10:58 +0300

    fixed the 3 clients 1 FD problem I had and all seg faults

--- server/src/TCPServer.cpp
+++ server/src/TCPServer.cpp
@@ -4,8 +4,6 @@
 
 #include "TCPServer.h"
 
-void *connectionHandler(void *arg);
-
 void *clientHandler(void *arg);
 
 TCPServer::TCPServer(const char *ip, int port) : Server(AF_INET, SOCK_STREAM, 0) {
@@ -14,10 +12,17 @@ TCPServer::TCPServer(const char *ip, int port) : Server(AF_INET, SOCK_STREAM, 0)
 }
 
 void TCPServer::start() {
-    pthread_t connectionThread;
-    struct ServerData data = {.server=m_socket, .targets=std::list<void *>()};
-    pthread_create(&connectionThread, nullptr, connectionHandler, &data);
-    pthread_join(connectionThread, nullptr);
+    pthread_t clientThread;
+    struct TCPServerData serverData = {.targets=std::list<Socket>(), .serverSocket=m_socket};
+    while (true) {
+        std::tuple<Socket, const char *> client = serverData.serverSocket.accept();
+        auto clientSocket = std::get<0>(client);
+        serverData.targets.push_back(clientSocket);
+        struct TCPClientData clientData = {.serverData=&serverData, .clientSocket=clientSocket};
+        if (pthread_create(&clientThread, nullptr, clientHandler, &clientData)) {
+            clientSocket.close();
+        }
+    }
 }
 
 size_t TCPServer::send(void *target, char *buffer, size_t length, int flags) {
@@ -30,51 +35,30 @@ size_t TCPServer::receive(void *target, char *buffer, size_t length, int flags) 
     return client.recv(buffer, length, flags);
 }
 
-void TCPServer::removeClient(struct ServerData *serverData, void *client) {
-    Socket* clientSocket = (Socket*)client;
-    for (auto *conn : serverData->targets) {
-        if (conn == client) {
-            serverData->targets.remove(conn);
-            clientSocket->close();
-        }
-    }
-}
-
-void *connectionHandler(void *arg) {
-    auto *serverData = (struct ServerData *) arg;
-    while (true) {
-        std::tuple<Socket, const char *> client = serverData->server.accept();
-        Socket clientSocket = std::get<0>(client);
-        serverData->targets.push_back(&clientSocket);
-        const char *clientAddress = std::get<1>(client);
-        std::cout << "Received connection from " << clientAddress << std::endl;
-
-        struct ClientData clientData = {.data=serverData, .client=&clientSocket};
-        pthread_t clientThread;
-        pthread_create(&clientThread, nullptr, clientHandler, &clientData);
-    }
-    return nullptr;
-}
-
 void *clientHandler(void *arg) {
-    auto *clientData = (struct ClientData *) arg;
-    char buffer[BUFSIZ];
+    auto *clientData = (struct TCPClientData *) arg;
+    auto serverData = clientData->serverData;
+    auto clientSocket = clientData->clientSocket;
     while (true) {
-        auto* clientSocket = (Socket*)clientData->client;
-        size_t received = clientSocket->recv(buffer, BUFSIZ - 1, 0);
-        buffer[received] = 0;
-        if (!(strcmp(buffer, "@exit"))) {
-            std::cout << "A client has disconnected" << std::endl;
-            TCPServer::removeClient(clientData->data, clientData->client);
-            break;
-        }
-        else {
-            std::cout << buffer << std::endl;
-            for (auto* conn : clientData->data->targets) {
-                if (conn != clientData->client) {
-                    clientSocket->send(buffer, received, 0);
+        try {
+            char buffer[BUFSIZ]{};
+            size_t received = clientSocket.recv(buffer, BUFSIZ - 1, 0);
+            if (!(strcmp(buffer, EXIT_MESSAGE)) || !received) {
+                serverData->targets.remove(clientSocket);
+                clientSocket.close();
+                break;
+            } else {
+                std::cout << buffer << std::endl;
+                for (auto conn : serverData->targets) {
+                    if (conn != clientSocket) {
+                        conn.send(buffer, received, 0);
+                    }
                 }
             }
+        }
+        catch (SocketException &e) {
+            std::cout << e.getMessage() << std::endl;
+            break;
         }
     }
     return nullptr;
